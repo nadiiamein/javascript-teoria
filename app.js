dc7baca26ab6c5c68aaf851c0599940be79470c6
Nadiia <npodufala15@gmail.com>
2020-11-23 21:50:36 +0200

    added console.log

--- app.js
+++ app.js
@@ -1,44 +1,46 @@
-//event Loop
+const heading = document.getElementById('hallo');
+//const heading2 = document.getElementsByTagName('h2-class')[0];
+//const heading2 = document.querySelector('.h2-class');
+const heading2 = document.querySelector('#sub-hallo');
+//const heading3 = heading2.nextElementSibling;
+const h2List = document.querySelectorAll('h2');
+console.log(h2List)
 
-const timeout = setTimeout (() => { 
-    console.log('After Timeout')}, 2500);
-    clearTimeout(timeout);
-    const interval = setInterval(() => { 
-        console.log('After Interval')}, 1000);
-         clearInterval(interval);
-        // const delay = (callback,wait = 1000) => {setTimeout(callback,wait)}
-        // delay(() => {
-        //     console.log('After 2 seconds')
-        // }, 2000); 
+const heading3 = h2List[h2List.length-1];
+console.log(heading3);
 
-        const delay = (wait = 1000) => {
-            const promise = new Promise( (resolve, reject) => {
-                setTimeout(() => {
-                  reject('Etwas gehts nicht wie muss')
-                    // resolve()
-                     },
-                    wait)
-            });
-            return promise;
-        };
+console.log(heading2);
+console.dir(heading.id);
+console.dir(heading.textContent);
 
-        // delay(2500)
-        // .then(() => {
-        //     console.log('After 2 seconds')
-        // })
-        // .catch(err => console.error('Error',err))
-        // .finally(() => console.log('Finally'));
+setTimeout( () => {
+addStylesTo(heading, 'Deutsch', 'red')}, 1500);
 
-        const getData = () => new Promise(resolve => resolve([1,1,2,3,5,8,13]))
-        getData().then(data => console.log(data));
-        async function asyncExample() {
-            try {
-            await delay (3000)
-            const data = await getData()
-            console.log('Data', data)
-            } catch (e) {
-                console.log(e);
-            }
-        }
+setTimeout( () => {
+    addStylesTo(heading2, 'Und commt alles dabei','orange', '2rem')}, 4000);
 
-        asyncExample();
+
+setTimeout( () => {
+    addStylesTo(heading3, 'Praktika')}, 3000);
+
+function addStylesTo(node, text, color = 'black', fontSize = '1rem') {
+    node.textContent = text;
+    node.style.color = color;
+    node.style.textAlign ='center';
+    node.style.backgroundColor ='yellow';
+    node.style.padding = '2rem';
+    // falsy: '', undefind, null,0,false
+    if (fontSize) {
+        node.style.fontSize = fontSize;
+    }
+}
+
+heading.onclick = () => {
+    if(heading.style.color === 'red') {
+        heading.style.color = '#000';
+        heading.style.backgroundColor = 'red';
+    } else {
+        heading.style.color = 'red';
+        heading.style.backgroundColor = 'yellow';
+    }
+};
